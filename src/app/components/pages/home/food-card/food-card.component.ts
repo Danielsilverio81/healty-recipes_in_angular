c15401108eaf1a35f45c8e7a0a7588d19a9afc19
danielsdev81 <danielsilverio81@utlook.com>
2024-04-14 18:26:19 -0300

    add new component recipes

--- src/app/components/pages/home/food-card/food-card.component.ts
+++ src/app/components/pages/home/food-card/food-card.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { RandomRecipesService } from '../../../../services/random-recipes/random-recipes.service';
+import { finalize } from 'rxjs';
 
 @Component({
   selector: 'app-food-card',
@@ -9,5 +11,33 @@ export class FoodCardComponent implements OnInit {
   public foods: any = [];
   public loading: boolean = true;
   public loadingMore: boolean = false;
-  ngOnInit(): void {}
+
+  constructor(private service: RandomRecipesService) {}
+  ngOnInit(): void {
+    this.getFoods();
+  }
+
+  getFoods() {
+    this.service
+      .listRandomFood(6)
+      .pipe(
+        finalize(() => {
+          this.loading = false;
+        })
+      )
+      .subscribe((data: any) => {
+        this.foods = this.foods.concat(data.recipes);
+      });
+  }
+
+  loadMore() {
+    this.loadingMore = true;
+    this.service.listRandomFood(3).pipe(
+      finalize(() => {
+        this.loadingMore = false;
+      })
+    ).subscribe((data:any)=> {
+      this.foods = this.foods.concat(data.recipes);
+    })
+  }
 }
--- src/app/services/random-recipes/random-recipes.service.ts
+++ src/app/services/random-recipes/random-recipes.service.ts
@@ -8,8 +8,7 @@ export class RandomRecipesService {
   constructor(private http: HttpClient) {}
 
   listRandomFood(take: number) {
-    const apiUrl = `https://api.spoonacular.com/recipes/random
-    ?apiKey=2a0f30fd5fa24116ae73c395ab807303&number=${take}`;
+    const apiUrl = `https://api.spoonacular.com/recipes/random?apiKey=2a0f30fd5fa24116ae73c395ab807303&number=${take}`;
 
     return this.http.get(apiUrl);
   }
